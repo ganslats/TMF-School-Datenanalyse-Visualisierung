7ea31fcc22eda91d1c577783535ab416e1f0d037
ganslats <ganslan@gmail.com>
2023-06-29 10:40:39 +0200

    R-Installationsscript ergänzt

--- install.R
+++ install.R
@@ -1,2 +1,2 @@
-packages <- c("DBI", "RSQLite", "dplyr", "dplyr", "forcats", "ggmap", "ggmap", "ggplot2", "grid", "gridExtra", "gtable", "icd.data", "leaflet", "leaflet", "lubridate", "readr", "readr", "sf", "stringr", "tidygeocoder", "tidyr", "tidyr", "zoo")
-install.packages(setdiff(packages, rownames(installed.packages())))
+packages <- c("curl", "DBI", "RSQLite", "dplyr", "dplyr", "forcats", "ggmap", "ggmap", "ggplot2", "grid", "gridExtra", "gtable", "icd.data", "leaflet", "leaflet", "lubridate", "readr", "readr", "sf", "stringr", "tidygeocoder", "tidyr", "tidyr", "zoo")
+install.packages(setdiff(packages, rownames(installed.packages())), repos="https://ftp.fau.de/cran/")
